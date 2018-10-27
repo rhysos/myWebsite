86f1d6ddb54a94f97d6c06f278f8a06964935bcf
rhysos <rhys.hooper1@gmail.com>
2018-10-28 08:08:38 +1300

    Update index.vue

--- amethyst/pages/index.vue
+++ amethyst/pages/index.vue
@@ -3,20 +3,13 @@
     <div>
       <app-logo/>
       <h1 class="title">
-        amethyst
+        Welcome to Amethyst Software
       </h1>
       <h2 class="subtitle">
-        Nuxt.js project
+        Bitrix24 Partner servicing NZ &amp; Australia
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+      <div class="page-content">
+        
       </div>
     </div>
   </section>
@@ -58,7 +51,7 @@ export default {
   padding-bottom: 15px;
 }
 
-.links {
+.page-content {
   padding-top: 15px;
 }
 </style>
